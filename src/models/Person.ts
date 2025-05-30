33bc22e08827ccb97332044bee9c8eca132d92bd
ashish <ashishrodio@gmail.com>
2025-05-31 02:35:28 +0530

    skipped lint for a line

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -12,7 +12,7 @@ interface PersonAttributes {
   id: number;
   name: string;
 }
-
+// eslint-disable-next-line
 interface PersonCreationAttributes extends Optional<PersonAttributes, "id"> {}
 
 @Table({
